5282aa37f3d1808cd64daf9edd73e6bb787aa881
Oksana Volkova <xeniavolchica@gmail.com>
2023-02-14 21:05:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args) {
+    int amount = 12780; // стоимость билета
+        int bonus = 20; // рублей в миле
+        int result = amount/bonus; // бонусные мили
+        System.out.println("Вам начислены бонусные мили:");
+        System.out.println(result);}
+
+}
