7b53c39c1d84ea263efe477d6c6f578fcbf37a1b
Oksana Volkova <xeniavolchica@gmail.com>
2023-02-15 20:08:25 +0300

    newchang src

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
-    public static void main (String[] args) {
-    int amount = 12780; // стоимость билета
+    public static void main(String[] args) {
+        int amount = 12780; // стоимость билета
         int bonus = 20; // рублей в миле
-        int result = amount/bonus; // бонусные мили
+        int result = amount / bonus; // бонусные мили
         System.out.println("Вам начислены бонусные мили:");
-        System.out.println(result);}
+        System.out.println(result);
+    }
 
 }
